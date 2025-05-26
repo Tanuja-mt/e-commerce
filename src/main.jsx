0e501cd21794cfad9401d7170c8ee53c7bc774a4
@tanuja.tiwari <tanuja.tiwari@squareyards.co.in>
2025-05-26 10:33:06 +0530

    ui changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode style={{background:'red'}}>
     <App />
   </StrictMode>,
 )
